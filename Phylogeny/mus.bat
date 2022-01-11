47fc433708ad1138b910842e7f1ee3ea09a869bd
Ryan Gourlie <52299431+fungal-spore@users.noreply.github.com>
2022-01-11 15:11:47 -0700

    Update mus.bat

--- Phylogeny/mus.bat
+++ Phylogeny/mus.bat
@@ -1,2 +1,3 @@
-#Align individual core gene-clusters with muscle
+#Align individual core gene-clusters with muscle;
+#I created a script to loop through all genes in directory but can't find atm
 ~/muscle/muscle3.8.31_i86linux64 -in $1  -out $1.out
