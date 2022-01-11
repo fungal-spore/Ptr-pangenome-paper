346babb530beabf2c7e0cc77fe3ca3e2029a2aa0
Ryan Gourlie <52299431+fungal-spore@users.noreply.github.com>
2022-01-11 15:14:52 -0700

    Update makeRAxMLtree.bat

--- Phylogeny/makeRAxMLtree.bat
+++ Phylogeny/makeRAxMLtree.bat
@@ -4,4 +4,4 @@ REM -m model
 REM -n outputname
 REM -p random seed
 REM -T #cores (ie threads)
-raxmlHPC-PTHREADS.exe -s tree1.fasta -m GTRCATX -n raxtest -p 10 -T 20
+raxmlHPC-PTHREADS.exe -s all_core.aln -m GTRCATX -n core.phylo -p 10 -T 20
