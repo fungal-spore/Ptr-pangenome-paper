d6d42d85d70f128ea515935d6ceb7ce4a38d5d52
Ryan Gourlie <52299431+fungal-spore@users.noreply.github.com>
2022-01-11 15:40:54 -0700

    Create TE_figures.R

---
+++ TransposableElements/TE_figures.R
@@ -0,0 +1,42 @@
+#Transposable Elements Ptr
+data <- read.table('Z:/User/genome/Ptr/pangenome/TE/TEsummary_forR4.csv', header = TRUE, sep=",")
+library(ggplot2)
+
+#graph by order
+data.order <- aggregate(data$perc_masked ~ data$Isolate*data$ORDER, FUN = 'sum')
+iso <- data.order$`data$Isolate`
+Class <- data.order$`data$ORDER`
+TE <- data.order$`data$perc_masked`
+
+ggplot(data.order,aes(x=iso,y=TE))+
+  geom_col(aes(fill=Class),width=0.7)  +
+  xlab("Isolate")+ylab("Percent TE content in genome")+
+  theme(axis.text.x = element_text(angle=45, hjust=1))
+
+#graph by class
+data.class <- aggregate(data$perc_masked ~ data$Isolate*data$CLASS, FUN = 'sum')
+iso <- data.class$`data$Isolate`
+Class <- data.class$`data$CLASS`
+TE <- data.class$`data$perc_masked`
+
+ggplot(data.class,aes(x=iso,y=TE))+
+  geom_col(aes(fill=Class),width=0.7)  +
+  xlab("Isolate")+ylab("Percent TE content in genome")+
+  theme(axis.text.x = element_text(angle=45, hjust=1))
+
+#graph by full
+cbPalette <- c("#999999", "#E69F00", "#56B4E9","#CC79A7", "#009E73", "#F0E442", "#0072B2", "#D55E00",  "red", "blue", "brown")
+
+data.code <- aggregate(data$perc_masked ~ data$Isolate*data$Full, FUN = 'sum')
+iso <- data.code$`data$Isolate`
+Class <- data.code$`data$Full`
+TE <- data.code$`data$perc_masked`
+
+ggplot(data.code,aes(x=iso,y=TE, fill=Class))+
+  geom_col(position="stack",width=0.7)  +
+  xlab("Isolate")+ylab("Percentage of genome with transposable elements")+
+  theme(text = element_text(size=14),
+  axis.text.x = element_text(angle=45, hjust=1))+
+  scale_fill_manual(values=cbPalette)
+
+
