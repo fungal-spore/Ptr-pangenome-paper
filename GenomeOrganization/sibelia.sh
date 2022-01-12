921b5cdd75f0169d90ebd4aa7554a2fa67899832
Ryan Gourlie <52299431+fungal-spore@users.noreply.github.com>
2022-01-12 11:46:41 -0700

    Update sibelia.sh

--- GenomeOrganization/sibelia.sh
+++ GenomeOrganization/sibelia.sh
@@ -1,4 +1,5 @@
 #Generate syntenic blocks with sibelia for use with Circos plotting
+#Should be able to run simply run Circos from output directory and it will generate plots
 #conda activate sibelia
 
 Sibelia -s loose $1 $2 -m 6500 -o ~/sibelia_output/
